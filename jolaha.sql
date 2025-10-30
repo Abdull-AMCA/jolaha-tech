ced8d5d6c01b8a8dd0d07494e476dbc5874d2de8
Abdull-AMCA <abdullabalaamca@gmail.com>
2025-10-30 15:39:07 +0400

    Re-exported SQL Database And Updated About-Hero Gradients
    Re-exported the database jolaha.sql file and also updated the gradient mesh for about-hero section

--- jolaha.sql
+++ jolaha.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2025 at 08:02 AM
+-- Generation Time: Oct 30, 2025 at 12:33 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `jolaha`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `careers`
+--
+
+CREATE TABLE `careers` (
+  `job_id` int(11) NOT NULL,
+  `job_title` varchar(255) NOT NULL,
+  `job_description` text NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `employment_type` enum('full-time','part-time','contract') NOT NULL,
+  `joining_date` date DEFAULT NULL,
+  `posted_date` datetime DEFAULT current_timestamp(),
+  `is_active` tinyint(1) DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `careers`
+--
+
+INSERT INTO `careers` (`job_id`, `job_title`, `job_description`, `location`, `employment_type`, `joining_date`, `posted_date`, `is_active`, `created_at`, `updated_at`) VALUES
+(1, 'Test', 'New', 'Dubai', 'full-time', '2025-10-16', '2025-10-29 14:56:30', 0, '2025-10-29 10:56:30', '2025-10-29 11:28:06'),
+(2, 'Adding New Career - Edited', 'Adding new career for testing pupose. Now editing it', 'Abu Dhabi', 'part-time', '2025-10-30', '2025-10-29 15:23:29', 1, '2025-10-29 11:23:29', '2025-10-29 11:27:22');
 
 -- --------------------------------------------------------
 
@@ -41,7 +68,12 @@ CREATE TABLE `newsletter_subscribers` (
 INSERT INTO `newsletter_subscribers` (`id`, `name`, `email`, `subscription_date`) VALUES
 (1, 'First', 'first@email.com', '2025-10-27 06:45:41'),
 (2, 'Second', 'second@email.com', '2025-10-27 06:49:14'),
-(3, 'Third', 'third@email.com', '2025-10-27 07:00:55');
+(3, 'Third', 'third@email.com', '2025-10-27 07:00:55'),
+(4, 'Test', 'test@email.com', '2025-10-27 07:55:34'),
+(5, 'Hi', 'hi@email.com', '2025-10-27 08:16:45'),
+(6, 'Hello', 'hello@email.com', '2025-10-27 08:20:00'),
+(7, 'g', 'g@email.om', '2025-10-27 08:22:51'),
+(8, 'New', 'New@email.com', '2025-10-27 08:27:15');
 
 -- --------------------------------------------------------
 
@@ -97,8 +129,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `created_at`, `updated_at`) VALUES
-(1, 'Product-1', '2025-10-15 13:32:33', '2025-10-15 13:32:45'),
-(2, 'Product-2', '2025-10-15 13:32:59', '2025-10-15 13:32:59');
+(1, 'Product-1', '2025-10-15 13:32:33', '2025-10-15 13:32:45');
 
 -- --------------------------------------------------------
 
@@ -121,8 +152,40 @@ CREATE TABLE `services` (
 --
 
 INSERT INTO `services` (`id`, `service_name`, `service_description`, `service_icon`, `is_active`, `created_at`, `updated_at`) VALUES
-(1, 'Service-1', 'Some service here', '', 1, '2025-10-15 13:35:03', '2025-10-15 13:35:03'),
-(2, 'Service-1', 'Some service here', '', 0, '2025-10-15 13:35:09', '2025-10-15 23:14:09');
+(1, 'Service-1', 'Some service here', '', 0, '2025-10-15 13:35:03', '2025-10-28 13:14:56'),
+(3, 'Service-2', 'Description of service 2', '', 0, '2025-10-28 08:52:43', '2025-10-28 13:14:24'),
+(5, 'Test Service', '', '', 0, '2025-10-28 12:34:59', '2025-10-28 13:04:59'),
+(6, 'Testing', 'Testing service and sub service deletion', '', 0, '2025-10-28 13:15:57', '2025-10-28 13:41:57'),
+(7, 'New Service', 'Again testing deletion till now', '', 0, '2025-10-28 14:01:27', '2025-10-28 14:12:26'),
+(8, 'New Service', 'New sub service', '', 0, '2025-10-28 14:19:06', '2025-10-28 14:32:31'),
+(9, 'Testing Sub S', '', '', 0, '2025-10-28 14:20:32', '2025-10-28 14:21:23'),
+(10, 'Testing Sub S', '', '', 0, '2025-10-28 14:20:38', '2025-10-28 14:27:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_inquiries`
+--
+
+CREATE TABLE `service_inquiries` (
+  `id` int(11) NOT NULL,
+  `service_type` varchar(100) NOT NULL,
+  `full_name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `company_name` varchar(150) DEFAULT NULL,
+  `project_type` varchar(100) DEFAULT NULL,
+  `budget_range` varchar(100) DEFAULT NULL,
+  `timeline` varchar(100) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `submitted_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `service_inquiries`
+--
+
+INSERT INTO `service_inquiries` (`id`, `service_type`, `full_name`, `email`, `company_name`, `project_type`, `budget_range`, `timeline`, `description`, `submitted_at`) VALUES
+(1, 'Web Design &amp; Development', 'First', 'first@email.com', 'First Company', 'New Web', '10,000', '1 month', 'Nothing to tell.', '2025-10-27 10:21:05');
 
 -- --------------------------------------------------------
 
@@ -143,7 +206,7 @@ CREATE TABLE `solutions` (
 
 INSERT INTO `solutions` (`solution_id`, `solution_name`, `created_at`, `updated_at`) VALUES
 (1, 'Solution-1', '2025-10-15 13:33:22', '2025-10-15 13:33:22'),
-(2, 'Solution-2', '2025-10-15 13:33:29', '2025-10-15 13:33:29');
+(5, 'New Soln', '2025-10-28 08:25:56', '2025-10-28 08:25:56');
 
 -- --------------------------------------------------------
 
@@ -166,16 +229,49 @@ CREATE TABLE `sub_services` (
 --
 
 INSERT INTO `sub_services` (`id`, `service_id`, `sub_service_name`, `sub_service_description`, `is_active`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Sub-S-1', '', 1, '2025-10-15 13:35:03', '2025-10-15 13:35:03'),
-(2, 1, 'Sub-S-2', '', 1, '2025-10-15 13:35:03', '2025-10-15 13:35:03'),
-(3, 1, 'Sub-S-3', '', 1, '2025-10-15 13:35:03', '2025-10-15 13:35:03'),
-(4, 2, 'Sub-S-1', '', 0, '2025-10-15 13:35:09', '2025-10-15 23:14:09'),
-(5, 2, 'Sub-S-2', '', 0, '2025-10-15 13:35:09', '2025-10-15 23:14:09'),
-(6, 2, 'Sub-S-3', '', 0, '2025-10-15 13:35:09', '2025-10-15 23:14:09');
+(55, 7, 'Sub S-1', '', 0, '2025-10-28 14:01:27', '2025-10-28 14:12:26'),
+(56, 7, 'Sub S-2', '', 0, '2025-10-28 14:01:27', '2025-10-28 14:12:26'),
+(57, 7, 'Sub S-1', '', 0, '2025-10-28 14:01:50', '2025-10-28 14:12:26'),
+(58, 7, 'Sub S-2', '', 0, '2025-10-28 14:01:50', '2025-10-28 14:12:26'),
+(59, 7, 'Sub S-3', '', 0, '2025-10-28 14:01:50', '2025-10-28 14:12:26'),
+(60, 7, 'Sub S-1', '', 0, '2025-10-28 14:02:08', '2025-10-28 14:12:26'),
+(61, 7, 'Sub S-1', '', 0, '2025-10-28 14:02:08', '2025-10-28 14:12:26'),
+(62, 7, 'Sub S-2', '', 0, '2025-10-28 14:02:08', '2025-10-28 14:12:26'),
+(63, 7, 'Sub S-2', '', 0, '2025-10-28 14:02:08', '2025-10-28 14:12:26'),
+(64, 7, 'Sub S-3', '', 0, '2025-10-28 14:02:08', '2025-10-28 14:12:26'),
+(65, 7, 'Sub S-1', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(66, 7, 'Sub S-1', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(67, 7, 'Sub S-1', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(68, 7, 'Sub S-1', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(69, 7, 'Sub S-2', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(70, 7, 'Sub S-2', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(71, 7, 'Sub S-2', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(72, 7, 'Sub S-2', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(73, 7, 'Sub S-3', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(74, 7, 'Sub S-3', '', 0, '2025-10-28 14:06:31', '2025-10-28 14:12:26'),
+(75, 8, 'Sub service', '', 0, '2025-10-28 14:19:06', '2025-10-28 14:32:31'),
+(76, 8, 'Sub service', '', 0, '2025-10-28 14:19:57', '2025-10-28 14:32:31'),
+(77, 8, 'Another', '', 0, '2025-10-28 14:19:57', '2025-10-28 14:32:31'),
+(78, 8, 'Yet another', '', 0, '2025-10-28 14:19:57', '2025-10-28 14:32:31'),
+(79, 9, 'Yes test', '', 0, '2025-10-28 14:20:32', '2025-10-28 14:21:23'),
+(80, 10, 'Yes test', '', 0, '2025-10-28 14:20:38', '2025-10-28 14:27:50'),
+(81, 10, 'ggg', '', 0, '2025-10-28 14:21:00', '2025-10-28 14:27:50'),
+(82, 10, 'ggg', '', 0, '2025-10-28 14:21:17', '2025-10-28 14:27:50'),
+(83, 10, 'Yes test', '', 0, '2025-10-28 14:21:17', '2025-10-28 14:27:50'),
+(84, 10, 'ggg', '', 0, '2025-10-28 14:26:41', '2025-10-28 14:27:50'),
+(85, 10, 'ggg', '', 0, '2025-10-28 14:26:41', '2025-10-28 14:27:50'),
+(86, 10, 'Yes test', '', 0, '2025-10-28 14:26:41', '2025-10-28 14:27:50'),
+(87, 10, 'Yes test', '', 0, '2025-10-28 14:26:41', '2025-10-28 14:27:50');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `careers`
+--
+ALTER TABLE `careers`
+  ADD PRIMARY KEY (`job_id`);
 
 --
 -- Indexes for table `newsletter_subscribers`
@@ -203,6 +299,12 @@ ALTER TABLE `services`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `service_inquiries`
+--
+ALTER TABLE `service_inquiries`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `solutions`
 --
 ALTER TABLE `solutions`
@@ -220,10 +322,16 @@ ALTER TABLE `sub_services`
 --
 
 --
+-- AUTO_INCREMENT for table `careers`
+--
+ALTER TABLE `careers`
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `newsletter_subscribers`
 --
 ALTER TABLE `newsletter_subscribers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `posts`
@@ -235,25 +343,31 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `service_inquiries`
+--
+ALTER TABLE `service_inquiries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `solutions`
 --
 ALTER TABLE `solutions`
-  MODIFY `solution_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `solution_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `sub_services`
 --
 ALTER TABLE `sub_services`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
 
 --
 -- Constraints for dumped tables
